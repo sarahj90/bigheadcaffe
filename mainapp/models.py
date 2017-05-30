fa14bb48b56ce4b378fa85490ca789ca9a59c9bb
bigheadcaffe <sarah.aljasser@hotmail.com>
2017-05-30 23:06:06 +0300

    models+views+forms

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+class Bean(models.Model):
+    name=models.CharField(max_length=200)
+    def __str__(self):
+        return (self.name)
+
+class Roast(models.Model):
+    name=models.CharField(max_length=200)
+    def __str__(self):
+        return (self.name)
+
+
+class Syrup(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return (self.name)
+
+class Powder(models.Model):
+    name=models.CharField(max_length=200)
+    def __str__(self):
+        return (self.name)
+
+
+class Coffee(models.Model):
+    user=models.ForeignKey(User)
+    name = models.CharField(max_length=200)
+    bean = models.ForeignKey(Bean)
+    roast = models.ForeignKey(Roast)
+    syrup = models.ManyToManyField(Syrup,blank=True)
+    powder = models.ManyToManyField(Powder, blank=True)
+    espresso_shots=models.PositiveIntegerField(default=1)
+    water=models.FloatField(default=0)
+    steamed_milk=models.BooleanField(default=False)
+    foam=models.FloatField(default=0)
+    extra_instructions=models.TextField()
+    def __str__(self):
+        return (self.name)
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,55 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .forms import *
 # Create your views here.
 
 
 def index(request):
-    return render (request,'/mainapp/home.html',{})
+    return render(request,'home.html',{})
 
 def login(request):
     return render(request,'login.html',{})
+
+def createCoffee(request):
+    context={}
+    if request.method == "POST":
+        form=CoffeeForm(request.POST)
+        context['form']=form
+        if form.is_valid():
+
+            coffee=form.save(commit=False)
+            coffee.user=request.user
+            coffee.save()
+            form.save_m2m()
+            return redirect("index")
+        else:
+            return render(request,'createCoffee.html',context)
+
+    else:
+        form=CoffeeForm()
+        context['form']=form
+        return render(request,'createCoffee.html',context)
+
+def editCoffee(request, coffee_id):
+    context={}
+    coffee=Coffee.objects.get(id=coffee_id)
+    context['coffee']=coffee
+    if request.method == "POST":
+        form=CoffeeForm(request.POST,instance=coffee)
+        context['form']=form
+        if form.is_valid():
+
+            coffee=form.save(commit=False)
+            coffee.user=request.user
+            coffee.save()
+            form.save_m2m()
+            return redirect("index")
+        else:
+            return render(request,'editCoffee.html',context)
+
+    else:
+        form=CoffeeForm(instance=coffee)
+        context['form']=form
+        return render(request,'editCoffee.html',context)
+
+
